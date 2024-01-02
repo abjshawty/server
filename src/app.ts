a8ff735b93865d1adc28efa71772d7805513371b
KOUADIO-BHEGNIN OTHNIEL KUAWO ANATOLE <othniel.kouadio@gs2e.ci>
2024-01-02 16:25:36 +0000

    helmet

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 // Imports
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 import { router } from './router';
 
 function init(){
@@ -10,12 +11,17 @@ function init(){
     // Allow Cross-Origin requests
     app.use(cors({ origin : true }));
 
+    // Security
+    app.use(helmet());
+
     // Allow JSON & Text in POST and PUT requests
     app.use(express.json());
     app.use(express.text({ type : 'text/html' }));
     
     // Add all the defined routes to the server
     app.use(router)
+
+    // Send the server with all parameters defined
     return app
 }
 
