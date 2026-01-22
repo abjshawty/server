fe7b770022de6a1f575d9890fed5811b8af40559
Timmy Kouadio-Bhegnin <kouadiobhegnino@gmail.com>
2026-01-22 16:28:29 +0000

    lockfile

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ new Elysia()
         ctx.set.headers['x-powered-by'] = 'Elysia';
         return ctx.status(418, "I'm a PNL teapot");
     })
-    .get('/docs', (ctx) => ctx.redirect("https://elysiajs.com"))
+    .get('/docs', (ctx) => ctx.redirect("https://elysiajs.com/table-of-content"))
     .get('/redirect', (ctx) => ctx.redirect("/docs"))
     .listen(3000);
