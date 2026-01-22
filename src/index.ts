58236c2e68c0193c26c7a6e7b11d60680e24ac7a
Timmy <othniel.kouadio@gs2e.ci>
2026-01-22 16:23:25 +0000

    Following the tutorial

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Not Implemented");
+import { Elysia } from 'elysia';
+
+new Elysia()
+    .get('/', (ctx) => {
+        ctx.set.headers['x-powered-by'] = 'Elysia';
+        return ctx.status(418, "I'm a PNL teapot");
+    })
+    .get('/docs', (ctx) => ctx.redirect("https://elysiajs.com"))
+    .get('/redirect', (ctx) => ctx.redirect("/docs"))
+    .listen(3000);
