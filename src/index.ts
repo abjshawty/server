b495f931d45b2632110974edc8722252f23aa3e8
Timmy <othniel.kouadio@gs2e.ci>
2026-01-22 17:02:02 +0000

    Handler, Validation & Hooks

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
 
 new Elysia()
+    .onAfterHandle((ctx) => { // Interceptor hook
+        console.log(ctx);
+    })
     .get('/', (ctx) => {
         ctx.set.headers['x-powered-by'] = 'Elysia';
         return ctx.status(418, "I'm a PNL teapot");
     })
     .get('/docs', (ctx) => ctx.redirect("https://elysiajs.com/table-of-content"))
     .get('/redirect', (ctx) => ctx.redirect("/docs"))
+    .post(
+        '/user',
+        ({ body: { name } }) => `Hello ${name}!`,
+        {
+            body: t.Object({
+                name: t.Optional(t.String())
+            }),
+            beforeHandle: (ctx) => { // Local hook
+                ctx.set.headers['x-powered-by'] = 'Elysia';
+            },
+            error: (ctx) => {
+                console.error(ctx.error);
+                return ctx.status(400, "Invalid name");
+            }
+        }
+    )
     .listen(3000);
+
+// https://elysiajs.com/tutorial/getting-started/guard/
